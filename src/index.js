fd9ecc35117bded65b96a5d70a2d15957f393a6e
norainu <modeverv@gmail.com>
2018-10-10 13:20:26 +0900

    2018-10-10 13:20:25

--- src/index.js
+++ src/index.js
@@ -47,12 +47,21 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
 
+    jumpTO(step) {
+        
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -69,7 +78,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
-            stepNumber: 0,
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
         console.log(squares);
